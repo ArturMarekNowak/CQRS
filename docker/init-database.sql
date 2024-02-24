3b1b8ecf9a4565b0b64b63adacb7ada381d59164
ArturMarekNowak <arturmareknowal@gmail.com>
2024-02-24 13:30:37 +0100

    Fix privileges

--- docker/init-database.sql
+++ docker/init-database.sql
@@ -1,13 +1,9 @@
 ﻿CREATE DATABASE users;
-
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO PUBLIC;
+    
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
 SELECT pg_create_physical_replication_slot('replication_slot');
 
-
-CREATE USER ro_user WITH PASSWORD 'qwerty';
 CREATE USER rw_user WITH PASSWORD 'qwerty';
-GRANT ALL PRIVILEGES ON DATABASE users TO rw_user;
-GRANT SELECT ON DATABASE users TO ro_user;
-
-
-    
+CREATE USER ro_user WITH PASSWORD 'qwerty';
+GRANT rw_user TO ro_user;
